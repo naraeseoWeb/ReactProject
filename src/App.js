9c0623a3f4ce8ce7ebd6ed03a62501b32d373d79
Narea <naraeseoez@gmail.com>
2022-08-05 15:57:31 +0900

    class형 함수

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
+import React from 'react';
 
 function App() {
 
@@ -8,7 +9,10 @@ function App() {
   let [따봉, 따봉변경] = useState([0,0,0]);
   let [modal, setmodal] = useState(false);
   let [title, setTitle] = useState(0);
-  let [입력값, 입력값변경] = useState(' ');
+  let [입력값, 입력값변경] = useState('');
+
+  const today = new Date();
+  let date = today.toLocaleString();
 
   return (
     <div className="App">
@@ -29,20 +33,32 @@ function App() {
                   따봉변경(copy);
                   }}>👍
                 </span>
-                  { 따봉[i] } javasc
+                  { 따봉[i] }
               </h4>
               <p>2월 17일 발행</p>
+              <button onClick={()=>{
+                let copy = [...글제목];
+                copy.splice(i, 1);
+                글제목변경(copy);
+              }}>삭제</button>
+              <p>{date}</p>
           </div>
           )
         })
       }
 
-      <input onChange={(e)=>{ 입력값변경(e.target.value); console.log(입력값) }}></input>
+      <input onChange={(e)=>{ 입력값변경( e.target.value); }}/>
+      <button onClick={()=>{ 
+        let copy = [...글제목];
+        copy.unshift(입력값);
+        글제목변경(copy);
+       }}>글 발행</button>
 
       {
         modal == true ? <Modal color={'skyblue'} 글제목={글제목} title={title}
         /> : null
       }
+      <Modal2></Modal2>
 
     </div>
   );
@@ -56,6 +72,30 @@ function Modal(props){
       <p>상세내용</p>
     </div>
   )
+}
+
+class Modal2 extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      name : "kim",
+      age : 20
+    }
+  }
+  render(){
+    return (
+      <div>
+        <p>
+          안녕 {this.state.age} 
+        </p>
+        <button onClick={()=>{this.setState({age:21})}}> 
+          버튼
+        </button>
+      </div>
+
+    )
+  }
+
 }
 
 export default App;
